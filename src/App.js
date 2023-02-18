51373a3b4679eceeec24188376bc9112d74b481d
Andrei Pereira <48697839+andreiPereira@users.noreply.github.com>
2023-02-18 13:58:22 -0300

    Ajustado BannerMain

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import Menu from "./components/Menu";
+import dadosIniciais from './data/dados_iniciais.json'
+import BannerMain from './components/BannerMain'
+import Carousel from './components/Carousel'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <div>
         <Menu />
-        <h1> :) </h1>
+        
+        <BannerMain 
+          videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
+          url={dadosIniciais.categorias[0].videos[0].url}
+          videoDescription={"Os reis do FLOW"}
+        />
     </div>
   );
 }

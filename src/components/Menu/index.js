ae7e25623fe466b9e8b611893fa9ba6fe1d8ff2a
Andrei Pereira <48697839+andreiPereira@users.noreply.github.com>
2023-02-18 12:32:37 -0300

    Terminado Header $ npm styled components

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import Logo from '../../assets/img/Logo.png';
+import ButtonLink from './components/ButtonLink';
 import './Menu.css'
 
 function Menu() {
     return (
         <nav className='Menu'>
-            <a href='/'>
-                <img className="Logo" src={Logo} alt="Raplix Logo"/>
+            <a href="./">
+                <img className="Logo" src={Logo} alt="Raplix Logo" />
             </a>
-            <a className='ButtonLink' href="/">
-                Novo vídeo
-            </a>
+            <ButtonLink className="ButtonLink" href="./">Novo Vídeo</ButtonLink>
         </nav>
     )
 }

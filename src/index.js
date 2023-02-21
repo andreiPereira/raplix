04ad2076dd40cf5026ce4882d420bb11d0915986
Andrei Pereira <48697839+andreiPereira@users.noreply.github.com>
2023-02-21 18:38:34 -0300

    Ajustando route atualizado

--- src/index.js
+++ src/index.js
@@ -3,9 +3,25 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />
+  },
+  {
+    path: "cadastro/video",
+    element: <div>Cadastro de Video = D</div>,
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
 );
